49a7190878badaa603f885c4b4d381b995232051
Elias Carmiol <elias@Eliass-MacBook-Pro.local>
2025-08-11 00:47:37 -0600

    Agregado el dropdown con la opcion de tema en header

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,16 @@
+// Agrega esto cerca de otras rutas
+
+//const listaRoutes = require('./routes/lista.routes');
+//app.use('/api/listas', listaRoutes);
+
+//Accesibilidad
+const tema = localStorage.getItem('tema', 'light');//Default es light
+const headTag = document.querySelector('html');
+if(headTag.attributes.length<2){
+    console.log("No data-theme attribute");
+    headTag.setAttribute('data-theme',tema);
+}
+//Cambiar el tema ejecutando el codigo de header.js
 // Cargar el header
 fetch('/frontend/components/header.html')
     .then(response => {
@@ -9,7 +22,6 @@ fetch('/frontend/components/header.html')
         }
     })
     .then(data => document.getElementById('header').innerHTML = data);
-
 // Cargar el footer
 fetch('/frontend/components/footer.html')
     .then(response => response.text())
