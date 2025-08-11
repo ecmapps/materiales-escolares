7dacf04036116b369c9445bd79f0e518f0568e85
joscr07 <jespinozav@ucenfotec.ac.cr>
2025-08-10 20:44:54 -0600

    Revert "Merge remote-tracking branch 'origin/iss5-lista-utiles-materiales' into iss5-lista-utiles-materiales"
    This reverts commit 5e6dfc89cc20bc6ac80a66c18fac4fbfe3b5d2db, reversing
    changes made to d350c6c23cb0d98c7ab282fccfd1bb065953ce3e.

--- backend/db.js
+++ backend/db.js
@@ -1,26 +1,40 @@
-require('dotenv').config({path:'env'});
+//require('dotenv').config({path:'env'});
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser'); //Permite interpretar los datos que vienen en la peticion en formto json
 //const rutasUsuarios = require(''); //folder de rutas
+require('dotenv').config();
 
 const aplicacion = express();
 
+//Importacion de las rutas (*ASOCIADO)
+// const usuarioRoute = require("./routes/usuario.route");
+// const productoRoute = require("./routes/producto.route");
+const categoriaRoute = require("./routes/categoria.route");
+
 // Middle
 aplicacion.use(express.json());
-console.log('Valor de mongodbURL: '+ process.env.MONGODB_URI);
-// Conexión a MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('Conexión exitosa a MongoDB'))
-.catch(err => console.error('Error al conectar con MongoDB:', err));
+aplicacion.use(bodyParser.urlencoded({extended:true}));
+aplicacion.use(bodyParser.json());//Habilita el analisis de JSON en las peticiones 
+aplicacion.use(cors());
 
-// Manejo de errores
-aplicacion.use((error, req, res, next) => {
-  console.error(error.stack);
-  res.status(500).json({
-    exito: false,
-    mensaje: 'Error interno del servidor'
-  });
+//Conexion al Servidor
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(()=> console.log('MongoDB Atlas conectado'))
+.catch(error => console.log('Ocurrio un error al conectarse con MongoDB: ', error));
+
+//Rutas (*ASOCIADO)
+// app.use("/usuarios", usuarioRoute);
+// app.use("/productos", productoRoute);
+aplicacion.use("/categorias", categoriaRoute);
+
+//Mensaje de conexione exitosa
+aplicacion.get('/', (req,res)=> {
+    res.send('Servidor en funcionamiento');
 });
 
 const PUERTO = process.env.PORT || 3000;
